fe2bcb867768e79a94cbe732ba33f06687a9ec46
Eyerusalem-hub <79621412+Eyerusalem-hub@users.noreply.github.com>
2022-12-09 18:45:55 -0600

    new

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Counter from "./compoments/counter.jsx"; //import the counter class, which returns a hello world
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //defines what shows on the screen
-  <React.StrictMode>
-    <Counter />
-  </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
